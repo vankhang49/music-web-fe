87b170fe936271d6d5f888031a25d4f0a9cca92d
vankhang49 <dvkhang49@gmail.com>
2024-10-02 08:21:51 +0700

    khangDV

--- src/pages/Album/Album.jsx
+++ src/pages/Album/Album.jsx
@@ -2,14 +2,14 @@ import {Avatar, Button, Card, Container, Flex, Grid, Group, Table, Typography} f
 import {usePlayMusic} from "../../core/contexts/PlayMusicContext";
 import {useParams} from "react-router-dom";
 import React, {useEffect, useState} from "react";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 import * as albumService from "../../core/services/AlbumService";
 import "./Album.css";
 import {albumsWantToListen} from "../../data/albumsWantToListen";
 import {sonTungAlbum} from "../../data/SonTungAlbum";
 import sontung from "../../assets/images/son-tung-mtp/sontung.jpg";
 
-export function Album(){
+export function Album() {
     const {
         playSongList,
         songIndexList,
@@ -20,31 +20,54 @@ export function Album(){
     } = usePlayMusic();
     const {id} = useParams();
     const [album, setAlbum] = useState({});
+    const [useStaticData, setUseStaticData] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
             await getAlbumById(id);
         }
         fetchData();
+        const timeout = setTimeout(() => {
+            setUseStaticData(true); // Switch to static data
+        }, 60000);
+
+        // Clean up the timeout when component unmounts
+        return () => clearTimeout(timeout);
     }, [id]);
 
     const getAlbumById = async (id) => {
-        const temp = await albumService.getAlbumById(id);
-        if (temp !== null) {
-            setAlbum(temp);
-        } else {
-            const tempAlbum = {
+        if (!useStaticData) {
+            const temp = await albumService.getAlbumById(id);
+            if (temp !== null) {
+                setAlbum(temp);
+            } else {
+                const tempAlbum = {
                     "albumId": 1,
-                    "title" : "Những Bài Hát Hay Nhất Của Sơn Tùng MTP",
-                    "artists" : [
+                    "title": "Những Bài Hát Hay Nhất Của Sơn Tùng MTP",
+                    "artists": [
                         {
-                            "artistId" : 2,
+                            "artistId": 2,
                             "artistName": "Sơn Tùng M-TP"
                         }
                     ],
                     "coverImageUrl": sontung,
                     "songs": sonTungAlbum,
                 };
+                setAlbum(tempAlbum);
+            }
+        } else {
+            const tempAlbum = {
+                "albumId": 1,
+                "title": "Những Bài Hát Hay Nhất Của Sơn Tùng MTP",
+                "artists": [
+                    {
+                        "artistId": 2,
+                        "artistName": "Sơn Tùng M-TP"
+                    }
+                ],
+                "coverImageUrl": sontung,
+                "songs": sonTungAlbum,
+            };
             setAlbum(tempAlbum);
         }
 
@@ -82,51 +105,58 @@ export function Album(){
             key: 'name',
             header: 'Bài hát',
             render: (row) => (<Card sizeImg={60} long={true} srcImg={row.coverImageUrl} title={row.title}
-                          description={row.artists?.map((artist, index) => (
-                            <Link key={artist.artistId}>
-                                {artist.artistName}
-                                {index !==  row.artists.length - 1 && <span>, </span>}
-                            </Link>))}/>
+                                    description={row.artists?.map((artist, index) => (
+                                        <Link key={artist.artistId}>
+                                            {artist.artistName}
+                                            {index !== row.artists.length - 1 && <span>, </span>}
+                                        </Link>))}/>
             ),
         },
         {
             key: 'duration',
             header: '',
             render: (row) =>
-                <Typography right tag="small" gd={{display: "block"}}>{((row.duration)/60).toFixed(2).replace('.', ':')}</Typography>,
+                <Typography right tag="small"
+                            gd={{display: "block"}}>{((row.duration) / 60).toFixed(2).replace('.', ':')}</Typography>,
         }
     ];
 
-    return(
-        <Container withShadow={false} gd={{ overflow: "hidden" }}>
-            <Flex justifyContent="space-between" alignItems="flex-start" flexWrap={'wrap'} >
+    return (
+        <Container withShadow={false} gd={{overflow: "hidden"}}>
+            <Flex justifyContent="space-between" alignItems="flex-start" flexWrap={'wrap'}>
                 <Group className={'album-card'}
-                    gd={{backgroundColor: 'transparent', textAlign: 'center', minWidth: '25%', maxWidth:'100%', width: '25%'}} >
+                       gd={{
+                           backgroundColor: 'transparent',
+                           textAlign: 'center',
+                           minWidth: '25%',
+                           maxWidth: '100%',
+                           width: '25%'
+                       }}>
                     <Card srcImg={album.coverImageUrl}
                           children={
-                            <Group>
-                                <Typography tag={"h1"}>{album.title}</Typography>
-                                <Flex center>
-                                    <Typography>Nghệ sĩ:</Typography>
-                                    <Typography>
-                                        {album.artists?.map((artist, index) => (
-                                            <a key={artist.artistId}>{artist.artistName}
-                                                {index !== album.artists.length - 1 && <span>, </span>}
-                                            </a>
-                                        ))}
-                                    </Typography>
-                                </Flex>
-                                <Typography>1 người yêu thích</Typography>
-                                <Flex center>
-                                    <Button onClick={handlePlayAlbum} text={'Phát ngẫu nhiên'}></Button>
-                                </Flex>
-                            </Group>
+                              <Group>
+                                  <Typography tag={"h1"}>{album.title}</Typography>
+                                  <Flex center>
+                                      <Typography>Nghệ sĩ:</Typography>
+                                      <Typography>
+                                          {album.artists?.map((artist, index) => (
+                                              <a key={artist.artistId}>{artist.artistName}
+                                                  {index !== album.artists.length - 1 && <span>, </span>}
+                                              </a>
+                                          ))}
+                                      </Typography>
+                                  </Flex>
+                                  <Typography>1 người yêu thích</Typography>
+                                  <Flex center>
+                                      <Button onClick={handlePlayAlbum} text={'Phát ngẫu nhiên'}></Button>
+                                  </Flex>
+                              </Group>
                           }
                     >
                     </Card>
                 </Group>
                 <Group className={'album-song-list'}
-                    gd={{backgroundColor: 'transparent', minWidth: '74%', maxWidth:'100%', width: '74%'}}>
+                       gd={{backgroundColor: 'transparent', minWidth: '74%', maxWidth: '100%', width: '74%'}}>
                     {album.songs && <Table border={false} columns={columns} data={album.songs} rowKey={"id"}
                                            className="custom-table" onClickRow={(index) => handlePlaySong(index)}
                     />}
@@ -140,7 +170,7 @@ export function Album(){
                               urlLink={`/artists/${artist.artistName}`}
                               LinkComponent={Link}
                               alt={artist.artistName} title={artist.artistName}
-                              description="55tr lượt xem" />
+                              description="55tr lượt xem"/>
                     ))}
                 </Grid>
             </Group>
